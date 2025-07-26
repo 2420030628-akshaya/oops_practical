333a63f5dff6d6cb874101fbb847e2553be789d9
2420030628-akshaya <2420030628@klh.edu.in>
2025-07-26 10:43:41 +0530

    Add files via upload

---
+++ oopspractical.java
@@ -0,0 +1,26 @@
+package cse;
+import java.util.Scanner;
+
+public class oopspractical {
+		public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter first integer: ");
+        int a = input.nextInt();
+        
+        System.out.print("Enter second integer: ");
+        int b = input.nextInt();
+        
+        System.out.println(); 
+        
+        System.out.println(" * Results * ");
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " % " + b + " = " + (a % b));
+        
+        input.close();
+    }
+}
+
